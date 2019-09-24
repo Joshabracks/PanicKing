068a747aed07149901f884a0b9e015b8d09090e4
Joshua Bracks <joshabracks@gmail.com>
2019-09-24 11:20:41 -0700

    Working on world generation

---
+++ game/gameObjects.js
@@ -0,0 +1,32 @@
+function Room(xCoord, yCoord){
+    this.location = {x: xcoord,y: ycoord};
+    //CONNECTED ROOMS BY THEIR COORDINATES
+    this.north = {
+        id = {x: this.xCoord, y: this.yCoord - 1}
+    };
+    this.south = {
+        id = {x: this.xCoord, y: this.yCoord + 1}
+    };
+    this.east = {
+        id = {x: this.xCoord + 1, y: this.yCoord}
+    };
+    this.west = {
+        id = {x: this.xCoord, y: this.yCoord - 1}
+    };
+    //CONTAINERS WITHIN THIS ROOM || RANDOMLY GENERATED
+    this.containers = {};
+}
+
+function Container() {
+    this.type;
+    this.room;
+    this.contents;
+    this.location;
+    this.image;
+}
+
+function Hat() {
+    this.type;
+    this.effect;
+    this.image;
+}
